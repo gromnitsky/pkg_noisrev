9d8b6f1b15d7e0696d812e837f382252e8a02872
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2011-08-05 13:23:04 +0300

    A log is created only if -v CLO is supplied.

--- lib/pkg_noisrev/meta.rb
+++ lib/pkg_noisrev/meta.rb
@@ -1,7 +1,7 @@
 module Pkg_noisrev
   module Meta
     NAME = 'pkg_noisrev'
-    VERSION = '0.0.1'
+    VERSION = '0.0.2'
     AUTHOR = 'Alexander Gromnitsky'
     EMAIL = 'alexander.gromnitsky@gmail.com'
     HOMEPAGE = 'http://github.com/gromnitsky/' + NAME
